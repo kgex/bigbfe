9d84a6894427c60514aab761e46d3f34fd880cd6
Navaneeth Malingan <navneetnivu07@gmail.com>
2022-11-11 18:35:40 +0530

    https

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const api = axios.create({
   // baseURL: 'http://127.0.0.1:8000',
   // baseURL: 'https://bigbbe.herokuapp.com',
-  baseURL: 'http://34.28.3.109:80'
+  baseURL: 'https://34.28.3.109:80'
 });
 
 
