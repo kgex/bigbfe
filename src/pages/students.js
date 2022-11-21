485349ee4a4254a748ca3fa91838834fcc8da272
Navaneeth Malingan <navneetnivu07@gmail.com>
2022-11-21 18:22:10 +0530

    students list added

--- src/pages/students.js
+++ src/pages/students.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import { Box, Container } from '@mui/material';
 import { StudentListResults } from '../components/student/students-list-results';
-import { StudentListToolbar } from '../components/student/student-list-toolbar';
 import { DashboardLayout } from '../components/dashboard-layout';
 import React, { useReducer, useState } from 'react';
 import api from '../utils/api';
@@ -14,25 +13,22 @@ const StudentList = (props) => {
 
   useEffect(() => {
 
-    console.log("Im here")
-
     const token = localStorage.getItem('token');
-    console.log(token);
-
     const user = localStorage.getItem('user');
     setUserId(JSON.parse(user).user_id);
 
-    console.log(userId)
     if (userId != null) {
       api.get(
-        `/users`, {
+        `/users/?skip=0&limit=200`, {
         headers: {
           'Authorization': `bearer ${token}`
         }
       })
         .then(res => {
-          console.log(res.data);
           setUserStudents(res.data);
+        }).catch(err => {
+          console.log("error log")
+          console.log(err);
         })
     }
 
@@ -54,7 +50,6 @@ const StudentList = (props) => {
         }}
       >
         <Container maxWidth={false}>
-          <StudentListToolbar />
           <Box sx={{ mt: 3 }}>
             <StudentListResults students={userStudents} />
           </Box>
