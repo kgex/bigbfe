d710479a18d050b694c6faf369e76ec7e2c53393
sathasivamthirumoorthi <sathasivamthirumoorthi@gmail.com>
2023-02-21 18:18:04 +0530

    api added

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   // baseURL: 'http://127.0.0.1:8000',
   // baseURL: 'https://bigbbe.herokuapp.com',
   // baseURL: 'https://34.28.3.109:80',
-  baseURL: 'https://bbapi.nivu.me'
+  baseURL: 'https://web-production-c873.up.railway.app'
 });
 
 api.interceptors.response.use(function (response) {
